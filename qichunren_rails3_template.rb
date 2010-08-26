51f9a60108fc12695adeb7489e113caf9f993f8c
Qichunren <whyruby@gmail.com>
2010-08-26 21:11:01 +0800

    add some template files

--- qichunren_rails3_template.rb
+++ qichunren_rails3_template.rb
@@ -4,6 +4,13 @@ run "rm public/index.html"
 run "rm README && touch README"
 route "root :to => \"welcome#index\""
 generate(:controller, "welcome", "index")
+run "cd app/models && wget  "
+
+if yes("Create a development database now?")
+  rake "db:create"
+  rake "db:migrate"
+end
 git :init
 git :add => "."
 git :commit => "-a -m 'Initial commit'"
+run "rails s"
