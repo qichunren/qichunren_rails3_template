b58fff8c4ba92b544b647cd479966b534d585c21
Qichunren <whyruby@gmail.com>
2010-09-30 23:09:26 +0800

    add authlogic ralis 3 branch

--- qichunren_rails3_template.rb
+++ qichunren_rails3_template.rb
@@ -1,5 +1,5 @@
 
-gem "authlogic",      "2.1.6"
+gem 'authlogic', :git => 'git://github.com/odorcicd/authlogic.git', :branch => 'rails3' 
 gem "will_paginate",  "3.0.pre2"
 gem 'friendly_id',    '~>3.0'
 gem 'acts-as-taggable-on', '2.0.6'
