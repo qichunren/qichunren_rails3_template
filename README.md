7b3120720fff6ee4f6c603c732290e6ece2f678b
qichunren <whyruby@gmail.com>
2011-01-25 20:55:31 +0800

    更新我的rails3项目模板

---
+++ README.md
@@ -0,0 +1,19 @@
+This is a template file for rails3 project that  for a common use.  
+
+### How to use it?
+
+      git clone git://github.com/qichunren/qichunren_rails3_template.git
+      rails new demo -TJ -d mysql -m qichunren_rails3_template/qichunren_rails3_template.rb
+      
+      
+### Things in the template
+
+- Install common gems, such as will_paginate, rails_config, mysql2
+- Remove initial README file, public/index.html and so on
+- Give you a initial rspec test environment
+- git initial commit 
+
+### Let me know more cool things
+
+If you know other cool things, such as cool gem, please tell me.
+   
--- qichunren_rails3_template.rb
+++ qichunren_rails3_template.rb
@@ -1,30 +1,41 @@
-
-gem 'authlogic', :git => 'git://github.com/odorcicd/authlogic.git', :branch => 'rails3' 
-gem "will_paginate",  "3.0.pre2"
-gem 'friendly_id',    '~>3.0'
-gem 'acts-as-taggable-on', '2.0.6'
-gem "rails_config",    '0.1.5'
+# install gems
+# gem 'authlogic', :git => 'git://github.com/odorcicd/authlogic.git', :branch => 'rails3' 
+gem 'mysql2'
+gem "will_paginate",        "3.0.pre2"
+gem 'friendly_id',          "~>3.0"
+gem 'acts-as-taggable-on',  "2.0.6"
+gem "rails_config",         "0.2.1"
 
 #gem "mongrel",                         :group => :development
-gem "awesome_print", :require => 'ap', :group => :development
+gem "awesome_print",                  :group => :development, :require => 'ap' 
 
-gem 'rspec-rails', '>=2.0.0',         :group => [:development, :test]
+gem "rspec", "~> 2.0",                :group => [:test, :development]
+gem 'rspec-rails', '>=2.0.0',         :group => [:development, :test] 
+gem "factory_girl_rails",             :group => [:test, :development]
 gem 'webrat',                         :group => [:development, :test]
 
-
-run "bundle install"
-
+# adjust some files
+run "rm README"
 run "rm public/index.html"
+run "rm public/images/rails.png"
+run "cp config/database.yml config/database.yml.example"
 
 generate "rspec:install"
-# generate "jquery:install --ui"   # it takes too much time
 
-generate(:controller, "welcome", "index")
+# jquery
+run "curl -L http://code.jquery.com/jquery-1.4.4.min.js > public/javascripts/jquery.js"
+run "curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js"
 
-route "root :to => \"welcome#index\"" # how to remove the route: get "welcome#index"
+append_file '.gitignore', <<-CODE
+config/database.yml
+Thumbs.db
+.DS_Store
+public/system/**
+log/
+tmp/*
+coverage/*
+CODE    
 
 git :init
 git :add => "."
 git :commit => "-a -m 'Initial commit'"
-
-puts "SUCCESS! Tip:rails g jquery:install --ui"
