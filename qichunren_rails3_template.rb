80e73f7fa518145fac18cd2711ab14b84427021a
qichunren <whyruby@gmail.com>
2011-04-30 14:48:01 +0800

    update some gems version

--- qichunren_rails3_template.rb
+++ qichunren_rails3_template.rb
@@ -1,21 +1,25 @@
 # install gems
-# gem 'authlogic', :git => 'git://github.com/odorcicd/authlogic.git', :branch => 'rails3' 
 gem 'mysql2'
 gem "will_paginate",        "3.0.pre2"
 gem 'friendly_id',          "~>3.0"
 gem 'acts-as-taggable-on',  "2.0.6"
 gem "rails_config",         "0.2.1"
 
-#gem "mongrel",                         :group => :development
+gem "capistrano"
+
 gem "awesome_print",                  :group => :development, :require => 'ap' 
 
-gem "rspec", "~> 2.0",                :group => [:test, :development]
-gem 'rspec-rails', '>=2.0.0',         :group => [:development, :test] 
+# for test
+gem "rspec", "~> 2.5",                :group => [:test, :development]
+gem 'rspec-rails', '>=2.5.0',         :group => [:development, :test] 
 gem "factory_girl_rails",             :group => [:test, :development]
 gem 'webrat',                         :group => [:development, :test]
 
 # adjust some files
 run "rm README"
+run "touch README.md"
+run "touch doc/RESOURCES.md"
+run "touch doc/TODO.md"
 run "rm public/index.html"
 run "rm public/images/rails.png"
 run "cp config/database.yml config/database.yml.example"
